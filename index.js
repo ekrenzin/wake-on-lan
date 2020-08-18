a32bcf94727ace61e51ff2c74fd434a1db9478bc
Ean Krenzin <ean@eankrenzin.com>
2020-08-18 12:42:34 -0700

    get add from options

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const udp = require('dgram');
  * @wiki https://en.wikipedia.org/wiki/Wake-on-LAN
  * @docs http://support.amd.com/TechDocs/20213.pdf
  */
-function createMagicPacket(mac) {
-  const MAC_REPEAT = 16;
-  const MAC_LENGTH = 0x06;
+function createMagicPacket(mac){
+  const MAC_REPEAT    = 16;
+  const MAC_LENGTH    = 0x06;
   const PACKET_HEADER = 0x06;
-  const parts = mac.match(/[0-9a-fA-F]{2}/g);
-  if (!parts || parts.length != MAC_LENGTH)
+  const parts  = mac.match(/[0-9a-fA-F]{2}/g);
+  if(!parts || parts.length != MAC_LENGTH)
     throw new Error(`malformed MAC address "${mac}"`);
   var buffer = Buffer.alloc(PACKET_HEADER);
   var bufMac = Buffer.from(parts.map(p => parseInt(p, 16)));
   buffer.fill(0xff);
-  for (var i = 0; i < MAC_REPEAT; i++) {
-    buffer = Buffer.concat([buffer, bufMac]);
+  for(var i = 0; i < MAC_REPEAT; i++){
+    buffer = Buffer.concat([ buffer, bufMac ]);
   }
   return buffer;
 };
@@ -29,34 +29,35 @@ function createMagicPacket(mac) {
  * @param  {Function} callback [description]
  * @return {[type]}            [description]
  */
-function wake(mac, address, options, callback) {
+function wake(mac, options, callback){
   options = options || {};
-  if (typeof options == 'function') {
+  if(typeof options == 'function'){
     callback = options;
   }
-  const { port } = Object.assign({
-    port: 9
+  const { address, port } = Object.assign({
+    address : '255.255.255.255',
+    port    : 9
   }, options);
   // create magic packet
   var magicPacket = createMagicPacket(mac);
   var socket = udp.createSocket(
     net.isIPv6(address) ? 'udp6' : 'udp4'
-  ).on('error', function (err) {
+  ).on('error', function(err){
     socket.close();
     callback && callback(err);
-  }).once('listening', function () {
+  }).once('listening', function(){
     socket.setBroadcast(true);
   });
   return new Promise((resolve, reject) => {
     socket.send(
       magicPacket, 0, magicPacket.length,
-      port, address, function (err, res) {
+      port, address, function(err, res){
         let result = res == magicPacket.length;
-        if (err) reject(err);
+        if(err) reject(err);
         else resolve(result);
         callback && callback(err, result);
         socket.close();
-      });
+    });
   });
 };
 
